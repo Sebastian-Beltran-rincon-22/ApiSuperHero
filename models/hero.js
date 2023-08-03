0f521292d42296babbce32ef630779a513d1bd75
sewastian <sebastianrincon834@gmail.com>
2023-08-03 15:33:34 -0500

    feat[models -> hero.js]

---
+++ models/hero.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const heroShema = new Schema({
+
+  heroId:{
+    type: Number,
+    required: true,
+    unique : true
+  },
+  namehero: {
+    type: String,
+    required : true,
+    maxLength : 30
+  },
+  imgHero: {
+    type: String,
+    required : false
+  },
+})
+
+const Hero = mongoose.model('Hero',heroShema)
+
+module.exports = Hero
+
