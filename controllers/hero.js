517fa732c7f18f0dfa669d8665ff77b424ba3062
sewastian <sebastianrincon834@gmail.com>
2023-08-04 16:04:27 -0500

    update [controllers -> hero.js]

--- controllers/hero.js
+++ controllers/hero.js
@@ -3,29 +3,30 @@ const Hero = require ('../models/hero')
 const heroControllers ={
 create: async (req,res) => {
   try {
-    const {namehero,imgHero} = req.body //(variables modelos que manejen mismo req.(algo), se pueden aregregar dentro {})
+    const {namehero,imgHero, done} = req.body //(variables modelos que manejen mismo req.(algo), se pueden aregregar dentro {})
     const lastHero = await Hero.findOne().sort({heroId: -1})
-    const newHeroId = lastHero ? lastHero.heroId +1 : +1
+    const newHeroId = lastHero ? lastHero.heroId +1 : 1
 
     const newHero = new Hero ({
       heroId: newHeroId,
       namehero : namehero,
-      imgHero : imgHero
+      imgHero : imgHero,
+      done:done
     })
     await newHero.save()
-    res.json({msg:'Created'})
+    res.status(201).json(newHero)
 
   }catch (error){
-    return res.status(500).json ({msg:error.message})
+    return res.status(409).json ({msg:error.message})
   }
   },
 get: async (req,res) => {
   try {
       const heros = await Hero.find({})
 
-      res.json(heros.reverse())
+      res.status(200).json(heros.reverse())
     } catch (error) {
-      return res.status(500).json({msg:error.message})
+      return res.status(204).json({msg:error.message})
     }
   },
 
@@ -33,26 +34,27 @@ getById: async (req,res) => {
     try {
         const hero = await Hero.findOne({heroId: req.params.heroId})
 
-        res.json(hero)
+        res.status(202).json(hero)
 
     } catch (error) {
-      return res.status(500).json({msg:error.message})
+      return res.status(204).json({msg:error.message})
     }
   },
 
 update: async (req,res) =>{
   try{
-    const {namehero, imgHero} = req.body
+    const {namehero, imgHero,done} = req.body
     const heroId = req.params
 
     await Hero.findOneAndUpdate(heroId,{
       namehero: namehero,
-      imgHero : imgHero
+      imgHero : imgHero,
+      done:done
     })
     res.json({msg:'updated'})
   }catch(error){
     console.error(error)
-    return res.status(500).json({msg:error.message})
+    return res.status(304).json({msg:error.message})
   }
 },
 
@@ -64,10 +66,9 @@ delete: async (req,res)=>{
       res.json({msg:'Deleted'})
   } catch (err) {
       console.error(err)
-      return res.status(500).json({msg:err.message})
+      return res.status(406).json({msg:err.message})
   }
 },
 }
-
 
 module.exports = heroControllers
